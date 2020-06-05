09aeb5b57de076363faedaf14c3ebbd49650178a
Habi95 <Habi95@users.noreply.github.com>
2020-06-05 17:55:05 +0200

    Extensions expanded

---
+++ C#/Breakfast/Breakfast/Breakfast.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Breakfast
+{
+    public class Breakfast
+    {
+
+        public static Coffee PourCoffee()
+        {
+            Coffee coffee = new Coffee();
+            return coffee;
+        }
+    }
+}
