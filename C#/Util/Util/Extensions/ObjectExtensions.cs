4db9d9029f62575761175c7f1d1577ea732ba060
Habi95 <Habi95@users.noreply.github.com>
2020-06-05 18:01:04 +0200

    add summary

--- C#/Util/Util/Extensions/ObjectExtensions.cs
+++ C#/Util/Util/Extensions/ObjectExtensions.cs
@@ -6,11 +6,21 @@ namespace Util.Extensions
 {
     class ObjectExtensions
     {
+        /// <summary>
+        /// Generate Objeect with Parameter {= Generateobject<T>(new object[] { ... } }
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="param"></param>
+        /// <returns></returns>
         public static T GenerateObject<T>(object[] param)
         {
             return (T)Activator.CreateInstance(typeof(T), param);
         }
-
+        /// <summary>
+        /// Generate Object without Parameter
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
         public static T NewObject<T>() where T : new()
         {
             return new T();
