ba0c3070acacab40243169484a217640f92cf251
Habi95 <Habi95@users.noreply.github.com>
2020-06-24 21:19:44 +0200

    token took right place but flashhhh

---
+++ C#/ConnectFour/ConnectFour/GameFrom.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace ConnectFour
+{
+    partial class GameFrom
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonRow1 = new System.Windows.Forms.Button();
+            this.buttonRow2 = new System.Windows.Forms.Button();
+            this.buttonRow7 = new System.Windows.Forms.Button();
+            this.buttonRow6 = new System.Windows.Forms.Button();
+            this.buttonRow5 = new System.Windows.Forms.Button();
+            this.buttonRow4 = new System.Windows.Forms.Button();
+            this.buttonRow3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonQuit = new System.Windows.Forms.Button();
+            this.labelHowIsMove = new System.Windows.Forms.Label();
+            this.labelPlayer2 = new System.Windows.Forms.Label();
+            this.labelPlayer1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonRow1
+            // 
+            this.buttonRow1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.buttonRow1.Location = new System.Drawing.Point(200, 110);
+            this.buttonRow1.Name = "buttonRow1";
+            this.buttonRow1.Size = new System.Drawing.Size(40, 40);
+            this.buttonRow1.TabIndex = 1;
+            this.buttonRow1.Text = "1";
+            this.buttonRow1.UseVisualStyleBackColor = true;
+            this.buttonRow1.Click += new System.EventHandler(this.button_Click);
+            // 
+            // buttonRow2
+            // 
+            this.buttonRow2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.buttonRow2.Location = new System.Drawing.Point(268, 110);
+            this.buttonRow2.Name = "buttonRow2";
+            this.buttonRow2.Size = new System.Drawing.Size(40, 40);
+            this.buttonRow2.TabIndex = 2;
+            this.buttonRow2.Text = "2";
+            this.buttonRow2.UseVisualStyleBackColor = true;
+            this.buttonRow2.Click += new System.EventHandler(this.button_Click);
+            // 
+            // buttonRow7
+            // 
+            this.buttonRow7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.buttonRow7.Location = new System.Drawing.Point(608, 110);
+            this.buttonRow7.Name = "buttonRow7";
+            this.buttonRow7.Size = new System.Drawing.Size(40, 40);
+            this.buttonRow7.TabIndex = 7;
+            this.buttonRow7.Text = "7";
+            this.buttonRow7.UseVisualStyleBackColor = true;
+            this.buttonRow7.Click += new System.EventHandler(this.button_Click);
+            // 
+            // buttonRow6
+            // 
+            this.buttonRow6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonRow6.Location = new System.Drawing.Point(540, 110);
+            this.buttonRow6.Name = "buttonRow6";
+            this.buttonRow6.Size = new System.Drawing.Size(40, 40);
+            this.buttonRow6.TabIndex = 6;
+            this.buttonRow6.Text = "6";
+            this.buttonRow6.UseVisualStyleBackColor = true;
+            this.buttonRow6.Click += new System.EventHandler(this.button_Click);
+            // 
+            // buttonRow5
+            // 
+            this.buttonRow5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonRow5.Location = new System.Drawing.Point(472, 110);
+            this.buttonRow5.Name = "buttonRow5";
+            this.buttonRow5.Size = new System.Drawing.Size(40, 40);
+            this.buttonRow5.TabIndex = 5;
+            this.buttonRow5.Text = "5";
+            this.buttonRow5.UseVisualStyleBackColor = true;
+            this.buttonRow5.Click += new System.EventHandler(this.button_Click);
+            // 
+            // buttonRow4
+            // 
+            this.buttonRow4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.buttonRow4.Location = new System.Drawing.Point(404, 110);
+            this.buttonRow4.Name = "buttonRow4";
+            this.buttonRow4.Size = new System.Drawing.Size(40, 40);
+            this.buttonRow4.TabIndex = 4;
+            this.buttonRow4.Text = "4";
+            this.buttonRow4.UseVisualStyleBackColor = true;
+            this.buttonRow4.Click += new System.EventHandler(this.button_Click);
+            // 
+            // buttonRow3
+            // 
+            this.buttonRow3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.buttonRow3.Location = new System.Drawing.Point(336, 110);
+            this.buttonRow3.Name = "buttonRow3";
+            this.buttonRow3.Size = new System.Drawing.Size(40, 40);
+            this.buttonRow3.TabIndex = 3;
+            this.buttonRow3.Text = "3";
+            this.buttonRow3.UseVisualStyleBackColor = true;
+            this.buttonRow3.Click += new System.EventHandler(this.button_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F);
+            this.label1.Location = new System.Drawing.Point(338, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(174, 46);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "00:00:00";
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.buttonStart.Location = new System.Drawing.Point(200, 12);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(84, 43);
+            this.buttonStart.TabIndex = 9;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            // 
+            // buttonQuit
+            // 
+            this.buttonQuit.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.buttonQuit.Location = new System.Drawing.Point(564, 12);
+            this.buttonQuit.Name = "buttonQuit";
+            this.buttonQuit.Size = new System.Drawing.Size(84, 43);
+            this.buttonQuit.TabIndex = 10;
+            this.buttonQuit.Text = "Quit";
+            this.buttonQuit.UseVisualStyleBackColor = true;
+            // 
+            // labelHowIsMove
+            // 
+            this.labelHowIsMove.AutoSize = true;
+            this.labelHowIsMove.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.labelHowIsMove.Location = new System.Drawing.Point(355, 66);
+            this.labelHowIsMove.Name = "labelHowIsMove";
+            this.labelHowIsMove.Size = new System.Drawing.Size(144, 25);
+            this.labelHowIsMove.TabIndex = 11;
+            this.labelHowIsMove.Text = "How is in Move";
+            // 
+            // labelPlayer2
+            // 
+            this.labelPlayer2.AutoSize = true;
+            this.labelPlayer2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.labelPlayer2.Location = new System.Drawing.Point(678, 66);
+            this.labelPlayer2.Name = "labelPlayer2";
+            this.labelPlayer2.Size = new System.Drawing.Size(140, 25);
+            this.labelPlayer2.TabIndex = 12;
+            this.labelPlayer2.Text = "Player 2 Name";
+            // 
+            // labelPlayer1
+            // 
+            this.labelPlayer1.AutoSize = true;
+            this.labelPlayer1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.labelPlayer1.Location = new System.Drawing.Point(23, 66);
+            this.labelPlayer1.Name = "labelPlayer1";
+            this.labelPlayer1.Size = new System.Drawing.Size(140, 25);
+            this.labelPlayer1.TabIndex = 13;
+            this.labelPlayer1.Text = "Player 1 Name";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.textBox1.Location = new System.Drawing.Point(23, 121);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(140, 30);
+            this.textBox1.TabIndex = 14;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.textBox2.Location = new System.Drawing.Point(683, 121);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(140, 30);
+            this.textBox2.TabIndex = 15;
+            // 
+            // GameFrom
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(875, 698);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.labelPlayer1);
+            this.Controls.Add(this.labelPlayer2);
+            this.Controls.Add(this.labelHowIsMove);
+            this.Controls.Add(this.buttonQuit);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonRow3);
+            this.Controls.Add(this.buttonRow4);
+            this.Controls.Add(this.buttonRow5);
+            this.Controls.Add(this.buttonRow6);
+            this.Controls.Add(this.buttonRow7);
+            this.Controls.Add(this.buttonRow2);
+            this.Controls.Add(this.buttonRow1);
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "GameFrom";
+            this.Text = "4_Wins!";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.GameFrom_Paint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button buttonRow1;
+        private System.Windows.Forms.Button buttonRow2;
+        private System.Windows.Forms.Button buttonRow7;
+        private System.Windows.Forms.Button buttonRow6;
+        private System.Windows.Forms.Button buttonRow5;
+        private System.Windows.Forms.Button buttonRow4;
+        private System.Windows.Forms.Button buttonRow3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button buttonQuit;
+        private System.Windows.Forms.Label labelHowIsMove;
+        private System.Windows.Forms.Label labelPlayer2;
+        private System.Windows.Forms.Label labelPlayer1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ C#/ConnectFour/ConnectFour/GameFrom.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConnectFour
+{
+    public partial class GameFrom : Form
+    {
+        private Rectangle[] boardColumns;
+        private int[,] board;
+        Color player1 = Color.Red;
+        Color player2 = Color.Yellow;
+        private int idOfPlayer = 1;
+        Graphics graph;
+        Rectangle rec;
+        public GameFrom()
+        {
+            InitializeComponent();
+            boardColumns = new Rectangle[7];
+            board = new int[6, 7];
+            graph = this.CreateGraphics();
+            //PrintBoard(null,null);
+
+
+        }
+
+       private void GameFrom_Paint(object sender, PaintEventArgs e)
+        {
+
+           PrintBoard(null, null);
+            /*e.Graphics.FillRectangle(Brushes.Black,200,160,450,350);
+            for (int columns = 0; columns < 6; columns++)
+            {
+                for (int rows = 0; rows < 7; rows++)
+                {
+                    if (columns == 0)
+                    {
+                        boardColumns[rows] = new Rectangle(32 + 48 * rows, 200, 160, 350);
+                    }
+                    e.Graphics.FillEllipse(Brushes.White, 200 + 68 * rows, 160 + 61 * columns, 43, 43);
+                    
+                }
+            }*/
+        }
+        private void PrintBoard(int? column, int? row)
+        {
+
+           
+
+
+            graph.FillRectangle(Brushes.Black, 200, 160, 450, 350);
+            for (int columns = 0; columns < 6; columns++)
+            {
+                for (int rows = 0; rows < 7; rows++)
+                {
+                    if (columns == 0)
+                    {
+                      rec =  boardColumns[rows] = new Rectangle(32 + 48 * rows, 200, 160, 350);
+                    }
+
+                    if (column.HasValue && row.HasValue)
+                    {
+                        if (column.Value == columns && row.Value == rows)
+                        {
+                             graph.FillEllipse(Brushes.Red, 200 + (68 * rows), 160 + (61 * columns), 43, 43);
+                        }
+                        else
+                        {
+                            graph.FillEllipse(Brushes.White, 200 + 68 * rows, 160 + 61 * columns, 43, 43);
+                        }
+                    }
+                    else
+                    {
+                        graph.FillEllipse(Brushes.White, 200 + 68 * rows, 160 + 61 * columns, 43, 43);
+                    }
+
+
+
+                }
+            }
+            
+                
+        }
+
+        
+
+       
+
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            int row = int.Parse(button.Text) - 1;
+            int? column = null;
+            for (int i = 5; i > 0; i--)
+            {
+                var k = board[i, row];
+                if (k == 0)
+                {
+                    column = i;
+                    board[i, row] = idOfPlayer;
+                    break;
+                }
+            }
+            PrintBoard(column, row);
+
+        }
+
+
+
+    }
+}
